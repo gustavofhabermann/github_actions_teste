107e2b283a6ea4be0beb6c103fa8b1d08d059eab
Gustavo Habermann <gustavo.francisco.habermann@gmail.com>
2025-09-20 12:05:01 -0300

    update
    Criação de 2 novos testes

--- testes/test_calculadora.py
+++ testes/test_calculadora.py
@@ -10,3 +10,11 @@ def test_dividir():
     assert dividir(10, 2) == 5
     with pytest.raises(ValueError):
         dividir(10, 0)  
+
+def test_subtrair():
+    assert subtracao(3,2) == 1
+    assert subtracao(0, 1) == -1
+
+def test_multiplicacao():
+    assert multiplicacao(2, 3) == 6
+    assert multiplicacao(0, 10) == 0 
